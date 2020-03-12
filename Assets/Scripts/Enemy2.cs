cd3eb252b51d0aa644ba464c6f101a5bdfef7532
jbrms <jeremycbrothers@gmail.com>
2020-03-12 13:13:28 -0400

    Moving some files from a test project to this one

---
+++ Assets/Scripts/Enemy2.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This is the base class for enemy behavior that should be applicable to all enemies in the game.
+/// This class handles basic enemy movement that can be refined in child classes if desired.
+///</summary>
+public class Enemy : MonoBehaviour
+{
+    protected Vector3 movement;
+    public struct EnemyData
+    {
+        float health;
+        float damage;
+
+        public EnemyData(float health, float damage)
+        {
+            this.health = health;
+            this.damage = damage;
+        }
+    }
+
+    // Increases speed by the factor set in the editor.
+    public float movementMultiplier = 1f;
+
+    // FixedUpdate is called a fixed number of times per second.
+    protected void FixedUpdate()
+    {
+        this.transform.Translate((movement * movementMultiplier) * Time.fixedDeltaTime);
+    }
+
+    ///<summary>
+    /// UpdateMovement base functionality flips a sprite if it has collided with a Collision2D object.
+    /// The sprite only flips if it collides with Collision2D objects with a tag of "VerticalCollider".
+    /// This function, when called used in a child class, should be called within MonoBehavior's OnCollisionEnter2D(Collision2D collision) function.
+    /// <param name="collision">A Collision2D object used that this class has collided with.</param>
+    ///</summary>
+    virtual protected void UpdateMovement(Collision2D collision)
+    {
+        // Get the tag set in the editor. If the collision's tag is 'VerticalCollider', or "Player", then determine which direction to flip.
+        if(collision.gameObject.tag == "VerticalCollider")
+        {
+            // flip the sprite and velocity
+            if(movement == Vector3.left)
+            {
+                movement = Vector3.right;
+                gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            }
+            else
+            {
+                movement = Vector3.left;
+                gameObject.GetComponent<SpriteRenderer>().flipX = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Skunk.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skunk : Enemy
+{
+    protected EnemyData enemyData;
+    private float currentSpeedMultiplier;
+    private bool canAttack = false;
+
+    // OnCollisionEnter2D is called whenever this object collides with some Collision2D object
+    protected void OnCollisionEnter2D(Collision2D collision)
+    {
+        UpdateMovement(collision);
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.tag == "Player")
+        {
+            movementMultiplier = 0;
+            canAttack = true;
+            PlayerCharacter player = collider.gameObject.GetComponent<PlayerCharacter>();
+            if(player)
+            {
+                Rigidbody2D playerRB = player.GetComponent<Rigidbody2D>();
+                if(playerRB)
+                {
+                    if(movement.x < 0 && this.gameObject.GetComponent<Renderer>().bounds.center.x > playerRB.GetComponent<Renderer>().bounds.center.x)
+                    {
+                        gameObject.GetComponent<SpriteRenderer>().flipX = true;
+                    }
+
+                    if(movement.x > 0 && this.gameObject.GetComponent<Renderer>().bounds.center.x < playerRB.GetComponent<Renderer>().bounds.center.x)
+                    {
+                        gameObject.GetComponent<SpriteRenderer>().flipX = false;
+                    }
+                }
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if(collider.gameObject.tag == "Player")
+        {
+            movementMultiplier = currentSpeedMultiplier;
+            canAttack = false;
+            if(movement.x < 0 && gameObject.GetComponent<SpriteRenderer>().flipX == true)
+            {
+                gameObject.GetComponent<SpriteRenderer>().flipX = false;
+            }
+            if(movement.x > 0 && gameObject.GetComponent<SpriteRenderer>().flipX == false)
+            {
+                gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = Vector3.left;
+        movementMultiplier = 1.75f;
+        enemyData = new EnemyData(10f,10f); // Set default values for the enemy data. Can also be changed in editor.
+        currentSpeedMultiplier = movementMultiplier;
+    }
+
+    // Update is called once per frame
+    protected new void FixedUpdate()
+    {
+        base.FixedUpdate();
+        if(canAttack)
+        {
+            Attack();
+        }
+         Debug.Log("Skunk vector: " + movement + "\nSkunk position: " + this.gameObject.GetComponent<Renderer>().bounds.center + "\n");
+    }
+
+    private void Attack()
+    {
+        
+    }
+}
