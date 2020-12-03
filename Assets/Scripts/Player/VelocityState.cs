ad20bc5d492c5a5139f4f3d6cfe1ffec554a9745
jeremycbrothers <jeremycbrothers@gmail.com>
2020-12-03 16:25:37 -0500

    Redoing the copy because I got the wrong commit before the end of the semester

--- Assets/Scripts/Player/VelocityState.cs
+++
@@ -1,6 +0,0 @@
-﻿///<summary>
-/// This enum is used for updating animation states in some animator component.
-/// It can be used for any sprite that can perform multiple actions defined below.
-/// (note) - it might be a good idea to change this to animation state?
-///</summary>
-public enum VelocityState { idle, walking, jumping, falling, attacking, hurt, crouch };
